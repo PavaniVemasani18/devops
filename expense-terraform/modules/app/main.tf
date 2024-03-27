35c424c314d728ae7e102d0720a8c2597db27783
Pavani <vemasani.pavani@gmail.com>
2024-03-27 17:28:11 +0530

    create a variable and load the message into tfvars

--- expense-terraform/modules/app/main.tf
+++ expense-terraform/modules/app/main.tf
@@ -1,6 +1,8 @@
 resource "null_resource" "test" {
   provisioner "local-exec" {
     command = "echo message ${var.message_m}"
+  }
+  provisioner "local-exec" {
     command = "echo message ${var.print_p}"
   }
 
