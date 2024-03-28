619032d1bc818867abea6f99ae452f6fad250c41
Pavani <vemasani.pavani@gmail.com>
2024-03-28 14:19:13 +0530

    create a bash file and run through bash

--- state-bucket/run.sh
+++ state-bucket/run.sh
@@ -1,4 +1,11 @@
 env=$1
+action=$2
+if [ -z $env ]; then
+  echo "env [dev/prod] is missing"
+fi
+if [ -z $action ]; then
+  echo "action [init/apply/destroy] is missing"
+fi
 rm -rf .terraform/terraform.tfstate
-terraform init -backend-config=env-$env/state-$env.tfvars
-terraform apply  -auto-approve
+terraform $action -backend-config=env-$env/state-$env.tfvars
+terraform $action  -auto-approve
