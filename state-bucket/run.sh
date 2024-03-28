de87b73c5e3dc567cbffbb57bad8ea9f0ed614e9
Pavani <vemasani.pavani@gmail.com>
2024-03-28 14:15:39 +0530

    create a bash file and run through bash

--- state-bucket/run.sh
+++ state-bucket/run.sh
@@ -1,4 +1,4 @@
 env=$1
 rm -rf .terraform/terraform.tfstate
-terraform init -backend-config=env-$env/state-dev.tfvars
+terraform init -backend-config=env-$env/state-$env.tfvars
 terraform apply  -auto-approve
