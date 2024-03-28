0fc16cd9c9a3b8c8fd73e15714ea888f50240632
Pavani <vemasani.pavani@gmail.com>
2024-03-28 13:21:33 +0530

    bucket name changed

--- state-bucket/state-file.tf
+++ state-bucket/state-file.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "buckt-tf-states"
+    bucket = "bknd-state-file"
     key    = "test1/file"
     region = "us-east-1"
   }
 }
-resource "null_resource" "test" {}
+//resource "null_resource" "test" {}
