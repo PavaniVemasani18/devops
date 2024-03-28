915e7840330455f9eb9d2f6bef84613f73a805c3
Pavani <vemasani.pavani@gmail.com>
2024-03-28 06:06:14 +0530

    create an instance

--- instanceCreation/sample.tf
+++ instanceCreation/sample.tf
@@ -2,9 +2,13 @@ resource "aws_instance" "test" {
   ami = var.ami
   instance_type = var.instancetype
   vpc_security_group_ids = [data.aws_security_group.security.id]
+  tags = {
+    name = var.instanceName
+  }
 }
 variable "ami" {}
 variable "instancetype"{}
+variable "instanceName"{}
 
 data "aws_security_group" "security"{
   name="allow-all"
