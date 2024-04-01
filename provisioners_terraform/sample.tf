92d40eedbe206151a0545a46c12157fbea2f94f1
Pavani <vemasani.pavani@gmail.com>
2024-04-01 16:33:47 +0530

    create a null resource instance for provisoner to iterate multiple times instance wont create

--- provisioners_terraform/sample.tf
+++ provisioners_terraform/sample.tf
@@ -1,24 +1,28 @@
 resource "aws_instance" "page" {
   ami = "ami-05f020f5935e52dc4"
   instance_type = "t3.small"
-  vpc_security_group_ids = [data.aws_security_group.security.id]
+  vpc_security_group_ids = [
+    data.aws_security_group.security.id]
   tags = {
-    Name= "page"
+    Name = "page"
   }
-  provisioner "remote-exec" {
-    connection {
-      type     = "ssh"
-      user     = "ec2-user"
-      password = "DevOps321"
-      host     = self.public_ip
-    }
-    inline = [
-      "dnf install nginx -y",
-      "sudo systemctl start nginx"
-    ]
+}
+resource "null_resource" "test" {
+provisioner "remote-exec" {
+  connection {
+    type     = "ssh"
+    user     = "ec2-user"
+    password = "DevOps321"
+    host     = aws_instance.page.public_ip
   }
+  inline = [
+    "sudo dnf install nginx -y",
+    "sudo systemctl start nginx"
+  ]
+}
 
 }
-data "aws_security_group" "security" {
+
+  data "aws_security_group" "security" {
   name = "allow-all"
 }
