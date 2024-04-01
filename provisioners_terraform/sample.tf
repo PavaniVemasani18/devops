8014a305e97ceca8f2446d37ebfb698b4306660d
Pavani <vemasani.pavani@gmail.com>
2024-04-01 16:12:42 +0530

    provisioner to install packages in current resource

--- provisioners_terraform/sample.tf
+++ provisioners_terraform/sample.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "page" {
   ami = "ami-05f020f5935e52dc4"
   instance_type = "t3.small"
-  vpc_security_group_ids = "[data.aws_security_group.security.id]"
+  vpc_security_group_ids = [data.aws_security_group.security.id]
 
   provisioner "remote-exec" {
     connection {
