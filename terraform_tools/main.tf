b81dabb2736a162d8bfd8e63003f95625e4c6af0
Pavani <vemasani.pavani@gmail.com>
2024-04-03 18:30:10 +0530

    load values in .tfvars and display those values on cmd prompt

--- terraform_tools/main.tf
+++ terraform_tools/main.tf
@@ -2,6 +2,6 @@ module "prometheus" {
   for_each = var.tools
   source = "./modules/app"
   tools_name = each.key
-  ami = each.value
-  instance_type = each.value
+  ami = each.value["ami"]
+  instance_type = each.value["instance_type"]
 }
