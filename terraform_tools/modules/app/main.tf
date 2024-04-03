d8b25953fefa60771f9b30d953697250269e81d8
Pavani <vemasani.pavani@gmail.com>
2024-04-03 18:06:27 +0530

    load values in .tfvars and display those values on cmd prompt

--- terraform_tools/modules/app/main.tf
+++ terraform_tools/modules/app/main.tf
@@ -1,9 +1,9 @@
 resource "aws_instance" "prometheus" {
-  ami = var.aws_ami
-  instance_type = var.aws_instanceType
+  ami = "var.aws_ami"
+  instance_type = "var.aws_instanceType"
   vpc_security_group_ids = [data.aws_security_group.security.id]
   tags = {
-    Name = var.tools_name
+    Name = "var.tools_name"
   }
 }
 data "aws_security_group" "security" {
